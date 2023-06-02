53d1ec9daaadecdadc690b5b7de4f56d79f2a81e
Akhil Binoy <92171031+akhil888binoy@users.noreply.github.com>
2023-06-02 20:04:13 +0530

    add header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
 
 function Header() {
-  return (
-    <div>Header</div>
-  )
+  return <nav>
+    <NavContent></NavContent>
+  </nav>
+};
+
+const  NavContent=()=>{
+    <>
+        <h2>Akhil Binoy</h2>
+        <div>
+            <a href="#home"> Home </a>
+            <a href="#work"> Work </a>
+            <a href="#timeline"> Experience </a>
+            <a href="#services"> Services </a>
+            <a href="#testimonial"> Testimonial </a>
+            <a href="#contact"> Contact </a>
+        </div>
+        <a href="mailto:official.akhil888binoy@gmail.com">
+            <button>Email</button>
+        </a>
+    
+    </>
 }
 
 export default Header
