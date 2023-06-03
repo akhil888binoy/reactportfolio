dd768725b57db64be39858e7a4ee47724bdf719a
Akhil Binoy <92171031+akhil888binoy@users.noreply.github.com>
2023-06-03 17:10:13 +0530

    configure timeline

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import data from "../assets/data.json"
 
 function Timeline() {
   return (
-    <div>Timeline</div>
+    <div id='timeline'>
+        <div className="timelineBox">
+
+        </div>
+    
+    </div>
   )
+}
+const TimelineItem=({heading,text, index})=>{
+    <div className= {`timelineItem ${
+        index%2===0}? "leftTimeline" : "rightTimeline"`}>
+            <h2>{heading}</h2>
+            <p>{text}</p>
+        </div>
 }
 
 export default Timeline
