b64fdc7de7788ade42781e9154be754c24c1176b
Akhil Binoy <92171031+akhil888binoy@users.noreply.github.com>
2023-06-03 16:22:52 +0530

    add timeline

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import Header from "./components/Header";
 import Home from "./components/Home";
+import Work from "./components/Work";
+import Timeline from "./components/Timeline";
 
 function App() {
   return (
     <>
       <Header></Header>
       <Home></Home>
+      <Work></Work>
+      <Timeline></Timeline>
     </>
   );
 }
