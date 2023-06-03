75d5e550ce7aa094990e43f6f4cfec5b7b3d1aa8
Akhil Binoy <92171031+akhil888binoy@users.noreply.github.com>
2023-06-03 20:13:16 +0530

    add styling

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { motion } from 'framer-motion'
+import  { AiFillAndroid, AiFillWindows, AiFillCiCircle} from "react-icons/ai"
 
 function Services() {
   return (
-    <div>Services</div>
+    <div id='services'><h2>Services</h2>
+    <section>
+        <motion.div className='serviceBox2'>
+            
+        </motion.div>
+
+    </section>
+    
+    </div>
   )
 }
 
